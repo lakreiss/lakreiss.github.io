699c8b54674a31084b9e0ce41dfc91076afb098b
Liam Kreiss <liam.kreiss@gmail.com>
2020-04-14 18:14:41 -0700

    starting to work, but has some bugs

--- ts/game_of_life.js
+++ ts/game_of_life.js
@@ -1,7 +1,7 @@
 var BOARD_HEIGHT = 20, BOARD_WIDTH = 22;
 var game_is_active = false;
 var interval; //not sure what type this is
-var cur_element, cur_tile, cur_row, cur_board, next_board;
+var cur_element, cur_tile_coords, num_neighbors, cur_tile, cur_row, cur_board, next_board;
 function get_tile_name(i, j) {
     return "tile" + String.fromCharCode(65 + j) + (i + 1).toString();
 }
@@ -29,16 +29,10 @@ function build_empty_board() {
 function toggle_tile(tile_name) {
     cur_element = document.getElementById(tile_name);
     cur_element.classList.toggle("tile_on");
-}
-function update_board() {
-    if (game_is_active) {
-        console.log("game is active");
-        //get next states
-        //set next states
-    }
-    else {
-        console.log("game is not active");
-    }
+    cur_tile_coords = get_tile_coords(tile_name);
+    // console.log(!cur_board[cur_tile_coords[0]][cur_tile_coords[1]]);
+    cur_board[cur_tile_coords[0]][cur_tile_coords[1]] = !cur_board[cur_tile_coords[0]][cur_tile_coords[1]];
+    // print_board();
 }
 function start_game() {
     if (game_is_active) {
@@ -50,7 +44,7 @@ function start_game() {
             //do nothing because an interval already exists
         }
         else {
-            setInterval(update_board, 33);
+            setInterval(update_board, 300);
         }
     }
 }
@@ -61,4 +55,75 @@ function stop_game() {
     else {
         game_is_active = false;
     }
+}
+function update_board() {
+    if (game_is_active) {
+        console.log("game is active");
+        //get next states
+        next_board = [];
+        for (var i = 0; i < BOARD_HEIGHT; i++) {
+            next_board.push([]);
+            for (var j = 0; j < BOARD_WIDTH; j++) {
+                num_neighbors = get_num_alive_neighbors(i, j);
+                if (cur_board[i][j]) { //cur tile is alive
+                    if (num_neighbors == 2 || num_neighbors == 3) {
+                        next_board[i][j] = true;
+                    }
+                    else {
+                        next_board[i][j] = false;
+                    }
+                }
+                else { //cur tile is dead
+                    if (num_neighbors == 3) {
+                        next_board[i][j] = true;
+                    }
+                    else {
+                        next_board[i][j] = false;
+                    }
+                }
+            }
+        }
+        //set next states //TODO write this
+        for (var i = 0; i < BOARD_HEIGHT; i++) {
+            for (var j = 0; j < BOARD_WIDTH; j++) {
+                if (next_board[i][j] != cur_board[i][j]) {
+                    cur_board[i][j] = next_board[i][j];
+                    toggle_tile(get_tile_name(i, j));
+                }
+            }
+        }
+    }
+    else {
+        console.log("game is not active");
+    }
+}
+function get_num_alive_neighbors(row, col) {
+    //TODO write this
+    var total_count = 0;
+    for (var i = -1; i <= 1; i++) {
+        for (var j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) {
+                //skip, main cell
+            }
+            else {
+                if (row + i < 0 || row + i >= BOARD_HEIGHT || col + j < 0 || col + j >= BOARD_WIDTH) {
+                    //skip, out of bounds
+                }
+                else {
+                    total_count += cur_board[row + i][col + j] ? 1 : 0;
+                }
+            }
+        }
+    }
+    return total_count;
+}
+function print_board() {
+    var output = "";
+    for (var i = 0; i < BOARD_HEIGHT; i++) {
+        output += "\n";
+        for (var j = 0; j < BOARD_WIDTH; j++) {
+            output += (cur_board[i][j] == true ? "T" : "f") + " ";
+        }
+    }
+    console.log(output);
 }
--- ts/game_of_life.ts
+++ ts/game_of_life.ts
@@ -1,7 +1,7 @@
 const BOARD_HEIGHT: number = 20, BOARD_WIDTH: number = 22;
 var game_is_active: boolean = false;
 var interval; //not sure what type this is
-var cur_element: Element, num_neighbors: number, cur_tile: string, cur_row: boolean[], cur_board: boolean[][], next_board: boolean[][];
+var cur_element: Element, cur_tile_coords: number[], num_neighbors: number, cur_tile: string, cur_row: boolean[], cur_board: boolean[][], next_board: boolean[][];
 
 function get_tile_name(i: number, j: number): string {
   return "tile" + String.fromCharCode(65+j) + (i+1).toString();
@@ -32,6 +32,10 @@ function build_empty_board(): void {
 function toggle_tile(tile_name: string): void {
   cur_element = document.getElementById(tile_name);
   cur_element.classList.toggle("tile_on");
+  cur_tile_coords = get_tile_coords(tile_name);
+  // console.log(!cur_board[cur_tile_coords[0]][cur_tile_coords[1]]);
+  cur_board[cur_tile_coords[0]][cur_tile_coords[1]] = !cur_board[cur_tile_coords[0]][cur_tile_coords[1]];
+  // print_board();
 }
 
 function start_game(): void {
@@ -42,7 +46,7 @@ function start_game(): void {
     if (interval) {
       //do nothing because an interval already exists
     } else {
-      setInterval(update_board, 33);
+      setInterval(update_board, 300);
     }
   }
 }
@@ -64,20 +68,66 @@ function update_board() {
     for (var i: number = 0; i < BOARD_HEIGHT; i++) {
       next_board.push([]);
       for (var j: number = 0; j < BOARD_WIDTH; j++) {
-        num_neighbors = get_num_neighbors(i, j);
-        //TODO add if statemtns for whether to set to true or false
-        next_board[i][j] = false;
+        num_neighbors = get_num_alive_neighbors(i, j);
+
+        if (cur_board[i][j]) { //cur tile is alive
+          if (num_neighbors == 2 || num_neighbors == 3) {
+            next_board[i][j] = true;
+          } else {
+            next_board[i][j] = false;
+          }
+        } else { //cur tile is dead
+          if (num_neighbors == 3) {
+            next_board[i][j] = true;
+          } else {
+            next_board[i][j] = false;
+          }
+        }
       }
     }
 
     //set next states //TODO write this
+    for (var i: number = 0; i < BOARD_HEIGHT; i++) {
+      for (var j: number = 0; j < BOARD_WIDTH; j++) {
+        if (next_board[i][j] != cur_board[i][j]) {
+          cur_board[i][j] = next_board[i][j];
+          toggle_tile(get_tile_name(i, j));
+        }
+      }
+    }
+
   } else {
     console.log("game is not active");
 
   }
 }
 
-function get_num_neighbors(row: number, col: number): number {
+function get_num_alive_neighbors(row: number, col: number): number {
   //TODO write this
-  return 0;
+  var total_count: number = 0;
+  for (var i: number = -1; i <= 1; i++) {
+    for (var j:number = -1; j <= 1; j++) {
+      if (i == 0 && j == 0) {
+        //skip, main cell
+      } else {
+        if (row + i < 0 || row + i >= BOARD_HEIGHT || col + j < 0 || col + j >= BOARD_WIDTH) {
+          //skip, out of bounds
+        } else {
+          total_count += cur_board[row + i][col + j] ? 1 : 0;
+        }
+      }
+    }
+  }
+  return total_count;
+}
+
+function print_board() {
+  var output: string = "";
+  for (var i: number = 0; i < BOARD_HEIGHT; i++) {
+    output += "\n";
+    for (var j: number = 0; j < BOARD_WIDTH; j++) {
+      output += (cur_board[i][j] == true ? "T" : "f") + " ";
+    }
+  }
+  console.log(output);
 }
