263cfc624e8b6d99bdd1de125d20701959fa304f
Liam Kreiss <liam.kreiss@gmail.com>
2020-07-24 15:57:13 -0700

    working on the game part

--- ts/lambsie.ts
+++ ts/lambsie.ts
@@ -1,28 +1,25 @@
 var lambsie_rows = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-var cur_turn = 0, STARTING_NUM_PLAYERS = 4;
-var num_players = 0; //temporary value
+var cur_turn = 0;
+var num_players = 4; //starting value
 
 function build_lamsbie_scorecard() {
 
     document.write("<form>")
     //create edit, save, and cancel buttons
-    document.write("<button class='lambsie_button' hidden>Edit</button>");
-    document.write("<button class='lambsie_button'>Save</button>");
-    document.write("<button class='lambsie_button' hidden>Cancel</button>");
-    document.write("<button class='lambsie_button'>Add Row</button>");
-    document.write("<button class='lambsie_button'>Remove Row</button>");
-    document.write('<br><br>')
-    document.write("<table>");
+    document.write("<button type='button' id='edit_button' class='lambsie_button' hidden>Edit</button>");
+    document.write("<button type='button' id='save_button' class='lambsie_button' hidden>Save</button>");
+    document.write("<button type='button' id='start_button' class='lambsie_button' onclick='start_game();'>Start Game</button>");
+    document.write("<button type='button' id='cancel_button' class='lambsie_button' hidden>Cancel</button>");
+    document.write("<button type='button' id='add_row_button' class='lambsie_button' onclick='add_row();'>Add Row</button>");
+    document.write("<button type='button' id='remove_row_button' class='lambsie_button' onclick='remove_row();'>Remove Row</button>");
+    document.write("<table id='lambsie_table'>");
 
     //create the player row
-    document.write('<tr><th class="bottom_lined">Players</th>');
-    for (var i = 0; i < STARTING_NUM_PLAYERS; i++) {
-        document.write('<td class="bottom_lined"><input type="text"id="player_' + i + '_name" size="6" placeholder="Name"></input></td>');
+    document.write('<tr><th>Players</th>');
+    for (var i = 0; i < num_players; i++) {
+        document.write('<td><input type="text" id="player_' + i + '_name_input" size="6" placeholder="Name"></input><span id="player_' + i + '_name" hidden></span></td>');
     }
     document.write('</tr>');
-
-    num_players = STARTING_NUM_PLAYERS; //filler, to be changed
-
 
     // create the number rows
     for (var i = 0; i < lambsie_rows.length; i++) {
@@ -35,9 +32,9 @@ function build_lamsbie_scorecard() {
     }
 
     //create the score total row
-    document.write('<tr><th class="top_lined">Total:</th>');
+    document.write('<tr><th>Total:</th>');
     for (var i = 0; i < num_players; i++) {
-        document.write('<td class="top_lined">0</td>')
+        document.write('<td>0</td>')
     }
     document.write('</tr>');
     
@@ -48,4 +45,44 @@ function build_lamsbie_scorecard() {
 
 function is_a_double_point_row(row) {
     return (row % 3 == 0)
+}
+
+function add_row() {
+    var table_rows = document.getElementById("lambsie_table").getElementsByTagName("tr");
+    var new_table_square;
+    var player_num = num_players; //this works because it's zero indexed
+    for (var i = 0; i < table_rows.length; i++) {
+        new_table_square = document.createElement("td");
+        if (i == 0) { //player name
+            new_table_square.innerHTML = '<input type="text" id="player_' + player_num + '_name_input" size="6" placeholder="Name"></input>'
+        } else if (i == 14) { //total
+            new_table_square.innerHTML = '0';
+        }
+        table_rows[i].appendChild(new_table_square);
+    }
+    num_players += 1;
+}
+
+function remove_row() {
+    if (num_players > 1) {
+        var table_rows = document.getElementById("lambsie_table").getElementsByTagName("tr");
+        for (var i = 0; i < table_rows.length; i++) {
+            var row = table_rows[i];
+            row.removeChild(row.childNodes[row.childNodes.length-1]);
+        }
+        num_players -= 1;
+    }
+}
+
+function start_game() {
+    //change the available buttons
+    document.getElementById('start_button').setAttribute('hidden', 'true');
+    document.getElementById('add_row_button').setAttribute('hidden', 'true');
+    document.getElementById('remove_row_button').setAttribute('hidden', 'true');
+    document.getElementById('save_button').removeAttribute('hidden');
+
+    //set the names
+    
+
+    //open up the forms for the Ace row
 }
