3a2cf885c02159a311102618f3cfd3de5b44ec19
Liam Kreiss <liam.kreiss@gmail.com>
2021-01-30 15:12:06 -0800

    added puzzle to my website from bianca's

---
+++ js/draggable.js
@@ -0,0 +1,126 @@
+//TAKEN FROM https://www.w3schools.com/howto/howto_js_draggable.asp
+
+var selected_element;
+var rotate_dict = {};
+var degree_increment = 3;
+
+window.addEventListener('keydown', function(e) { key_press(e); });
+
+function key_press(e) {
+  // alert(e.keyCode);
+  switch(e.keyCode) {
+    case 37: //left key
+      //maybe translate left in the future
+      break;
+    case 38: //up key
+    //maybe translate up in the future
+      break;
+    case 39: //right key
+    //maybe translate right in the future
+      break;
+    case 40: //down key
+    //maybe translate down in the future
+      break;
+    case 88: //x key
+      if (selected_element) {
+        rotate_selected_element("right");
+      }
+      break;
+    case 90: //z key
+      if (selected_element) {
+        rotate_selected_element("left");
+      }
+      break;
+    default:
+      // do nothing
+  }
+}
+
+function set_selected(id) {
+  if (selected_element) {
+    selected_element.classList.toggle("selected");
+  }
+  selected_element = document.getElementById(id);
+  selected_element.classList.toggle("selected");
+}
+
+function rotate_selected_element(direction) {
+  rotate_dict[selected_element.id] += (direction == 'right' ? degree_increment : (direction == 'left' ? -degree_increment : 0));
+  selected_element.style.transform = 'rotate('+rotate_dict[selected_element.id]+'deg)';
+}
+
+//Make the DIV element draggagle:
+var draggable_elements = document.getElementsByClassName("draggable");
+for (var i = 0; i < draggable_elements.length; i++) {
+  rotate_dict[draggable_elements[i].id] = 0;
+  dragElement(draggable_elements[i]);
+}
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+function clear_selected_element() {
+  selected_element.classList.toggle("selected");
+  selected_element = null;
+}
+
+document.getElementById('puzzle_container').onclick = clear_selected_element;
+
+//TODO: this does allow dragging but theres no rotating without a keyboard and it disables clicking, so i'm getting rid of it for now
+// function touchHandler(event) {
+//     var touch = event.changedTouches[0];
+//
+//     var simulatedEvent = document.createEvent("MouseEvent");
+//         simulatedEvent.initMouseEvent({
+//         touchstart: "mousedown",
+//         touchmove: "mousemove",
+//         touchend: "mouseup"
+//     }[event.type], true, true, window, 1,
+//         touch.screenX, touch.screenY,
+//         touch.clientX, touch.clientY, false,
+//         false, false, false, 0, null);
+//
+//     touch.target.dispatchEvent(simulatedEvent);
+//     event.preventDefault();
+// }
+//
+// function init() {
+//     document.addEventListener("touchstart", touchHandler, true);
+//     document.addEventListener("touchmove", touchHandler, true);
+//     document.addEventListener("touchend", touchHandler, true);
+//     document.addEventListener("touchcancel", touchHandler, true);
+// }
+//
+// init();
---
+++ js/puzzle.js
@@ -0,0 +1,79 @@
+var scale_factor = 7.56;
+var all_puzzle_piece_shapes = [
+    "0,0 10,0 10,10 30,10 30,20 10,20 10,30 0,30",
+    "0,0 10,0 10,10 20,10 20,20 10,20 10,40 0,40",
+    "0,0 10,0 10,10 20,10 20,20 30,20 30,30 10,30 10,20 0,20",
+    "0,0 10,0 10,10 30,10 30,20 20,20 20,30 10,30 10,20 0,20",
+    "0,0 10,0 10,10 30,10 30,20 20,20 20,30 10,30 10,20 0,20",
+];
+var all_puzzle_piece_colors = [
+    "lime",
+    "blue",
+    "green",
+    "red",
+    "pink"
+];
+function make_outline() {
+    document.write('<div><svg width="500" height="500">');
+    document.write('<rect x="20" y="10" width="450" height="450" style="fill:white;stroke:black;stroke-width:5;opacity:1" />');
+    document.write('</svg></div>');
+    /* <div>
+      <svg width="500" height="500">
+        <rect x="20" y="10" width="450" height="450"
+        style="fill:white;stroke:black;stroke-width:5;opacity:1" />
+      </svg>
+    </div> */
+}
+function make_pieces() {
+    /* <div id="block_0" class="draggable" onclick="set_selected('block_0')">
+      <svg width="120" height="120">
+        <polygon points="0,0 40,0 40,40 120,40 120,80 40,80 40,120 0,120"
+        style="fill:lime;" />
+      </svg>
+    </div>  */
+    for (var i = 0; i < all_puzzle_piece_shapes.length; i++) {
+        document.write('<div id="block_' + i + '" class="draggable" onclick="set_selected(\'block_' + i + '\')">');
+        var scaled_coords = get_scaled_coords(all_puzzle_piece_shapes[i]);
+        var scaled_width = get_scaled_width(scaled_coords);
+        var scaled_height = get_scaled_height(scaled_coords);
+        document.write('<svg width="' + scaled_width + '" height="' + scaled_height + '"> <polygon points="' + scaled_coords + '" style="fill:' + all_puzzle_piece_colors[i] + ';" /> </svg> </div>');
+    }
+}
+function get_scaled_width(points) {
+    var points_to_coords = points.split(" ");
+    var max_x = 0;
+    for (var i = 0; i < points_to_coords.length; i++) {
+        var coords = points_to_coords[i].split(",");
+        var x = parseInt(coords[0]);
+        if (x > max_x) {
+            max_x = x;
+        }
+    }
+    return max_x;
+}
+function get_scaled_height(points) {
+    var points_to_coords = points.split(" ");
+    var max_y = 0;
+    for (var i = 0; i < points_to_coords.length; i++) {
+        var coords = points_to_coords[i].split(",");
+        var y = parseInt(coords[1]);
+        if (y > max_y) {
+            max_y = y;
+        }
+    }
+    return max_y;
+}
+function get_scaled_coords(points) {
+    var points_to_coords = points.split(" ");
+    var points_to_return = '';
+    for (var i = 0; i < points_to_coords.length; i++) {
+        var coords = points_to_coords[i].split(",");
+        var x = parseInt(coords[0]), y = parseInt(coords[1]);
+        if (i != 0) {
+            points_to_return += " ";
+        }
+        points_to_return += x * scale_factor + "," + y * scale_factor;
+    }
+    return points_to_return;
+}
+//# sourceMappingURL=puzzle.js.map
